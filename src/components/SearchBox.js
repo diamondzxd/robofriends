aa750ff10264b9515e35dcdd2614d642940fefc2
Piyush Dhall <contact@piyush.ovh>
2022-11-12 21:25:56 +0530

    refactored code and put autoFocus on the search bar

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,10 +4,12 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pad2">
       <input
+        autoFocus
         onChange={searchChange}
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
+        style={{ marginBottom: "10px" }}
       ></input>
     </div>
   );
