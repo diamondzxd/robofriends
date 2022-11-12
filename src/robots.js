fe42022cd15df664166056c821b3f31c24a4a423
Piyush Dhall <contact@piyush.ovh>
2022-11-12 18:33:21 +0530

    card are now in the CardList

---
+++ src/robots.js
@@ -0,0 +1,22 @@
+export const robots = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@email.com",
+  },
+  {
+    id: 2,
+    name: "Piyush",
+    email: "contact@piyush.ovh",
+  },
+  {
+    id: 3,
+    name: "Diamondzxd",
+    email: "admin@diamondzxd.com",
+  },
+  {
+    id: 4,
+    name: "Boop Beep",
+    email: "boop@xyz.com",
+  },
+];
